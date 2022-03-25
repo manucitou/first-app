a08d43201b6bb88280384e10099a8fd258e40a2c
Manuel Moreira <manuel.moreira.valle@gmail.com>
2022-03-24 22:25:12 -0300

    first commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html'
+})
+
+export class ServerComponent {
+  serverID = 10;
+  serverStatus = 'offline';
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+}
